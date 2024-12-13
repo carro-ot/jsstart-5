119f037ca4a4e3e9e1c1d513739919ca25440016
carro-ot <168121508+carro-ot@users.noreply.github.com>
2024-12-13 20:40:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const product = [
+	{ name: 'T-shirt', price: 2000, category: 'Одежда' },
+	{ name: 'Ring', price: 30000, category: 'Аксессуары' },
+	{ name: 'Phone', price: 110000, category: 'Электроника' },
+	{ name: 'Socks', price: 200, category: 'Одежда' },
+	{ name: 'Bracelet', price: 12000, category: 'Аксессуары' },
+	{ name: 'Headphones', price: 10000, category: 'Электроника' },
+];
+
+
+
