422f676575c2096b7058b20f6169b56795f3ea17
carro-ot <168121508+carro-ot@users.noreply.github.com>
2024-12-16 14:14:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,5 +7,28 @@ const product = [
 	{ name: 'Headphones', price: 10000, category: 'Электроника' },
 ];
 
+const sortAsc = document.getElementById('sortAsc');
+const sortDesc = document.getElementById('sortDesc');
+const resetFilters = document.getElementById('resetFilters');
+
+const productList = document.getElementById('productList');
+
+const radioButton = document.querySelectorAll('input[type="radio"]');
+
+productList.innnerHTML = "";
+
+
+product.forEach(product => {
+	const li = document.createElement('li');
+	li.innerHTML = `
+		<string>${product.name}</string>
+		<p>Price: ${product.price}.rub</p>
+		<p>Category: ${product.category}</p>
+	`;
+	productList.appendChild(li);
+})
+
+// const clothe = product.filter(product => product.category === 'Одежда');
+// console.log(clothe);
 
 
