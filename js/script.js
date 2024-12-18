4561139ad1403923f52334a370fb1745fea3e58b
carro-ot <168121508+carro-ot@users.noreply.github.com>
2024-12-18 16:51:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,57 +7,123 @@ const product = [
 	{ name: 'Headphones', price: 10000, category: 'Электроника' },
 ];
 
-const sortAsc = document.getElementById('sortAsc');
-const sortDesc = document.getElementById('sortDesc');
-const resetFilters = document.getElementById('resetFilters');
-
 const productList = document.getElementById('productList');
+const priceRange = document.getElementById('priceRange');
+const priceValue = document.getElementById('priceValue');
+const resetButton = document.getElementById('resetFilters');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
 
-const radioButton = document.querySelectorAll('input[type="radio"]');
+function renderProducts(filteredProducts) {
 
-
-function renderProducts(products) {
-	const productList = document.getElementById('productList'); // Убедись, что ID совпадает
 	productList.innerHTML = '';
 
 
-	product.forEach(product => {
+	//создание списка элементов списка и проход по фильтрации товаров 
+	filteredProducts.filteredProducts.forEach(product => {
 		const li = document.createElement('li');
 		li.innerHTML = `
-		<string>${product.name}</string>
-		<p>Price: ${product.price}.rub</p>
-		<p>Category: ${product.category}</p>
-	`;
-		productList.appendChild(li);
+			<h3>${product.name}</h3>
+			<p>Цена:${product.price}</p>
+			<p>Категория:${product.category}</p>
+		`;
+		productList.appendChild('li');
 	});
 }
 
-const selectedCategory = document.querySelector('input[type="radio"]:checked').value;
-
-const radioButtons = document.querySelectorAll('input[type = "radio"]');
-radioButtons.forEach(button => {
-	button.addEventListener('change', () => {
-		const selectedCategory = document.querySelectorAll('input[type = "radio"]:checked').value;
-		const filteredProducts = filteredsProducts(product, selectedCategory);
-		const maxPrice = 50000;
-
-
-const filteredsProducts = product.filter(product => 
-	product.category === selectedCategory && product.price < maxPrice
-		);
+function filteredProducts() {
 		
-renderProducts(filteredsProducts);
-
-	});
-});
-
-
-
-  
+	}
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sortAsc = document.getElementById('sortAsc');
+// const sortDesc = document.getElementById('sortDesc');
+// const resetFilters = document.getElementById('resetFilters');
+
+// const productList = document.getElementById('productList');
+
+// const radioButton = document.querySelectorAll('input[type="radio"]');
+
+
+// function renderProducts(products) {
+// 	const productList = document.getElementById('productList'); // Убедись, что ID совпадает
+// 	productList.innerHTML = '';
+
+
+// 	product.forEach(product => {
+// 		const li = document.createElement('li');
+// 		li.innerHTML = `
+// 		<string>${product.name}</string>
+// 		<p>Price: ${product.price}.rub</p>
+// 		<p>Category: ${product.category}</p>
+// 	`;
+// 		productList.appendChild(li);
+// 	});
+// }
+
+
+// function filteredProducts() {
+// const selectedCategory = document.querySelector('input[type="radio"]:checked').value;
+// const radioButtons = document.querySelectorAll('input[type = "radio"]');
+// radioButtons.forEach(button => {
+// 	button.addEventListener('change', () => {
+// 		const selectedCategory = document.querySelectorAll('input[type = "radio"]:checked').value;
+// 		const filteredProducts = filteredsProducts(product, selectedCategory);
+// 		const maxPrice = 50000;
+
+
+// const filteredsProducts = product.filter(product => 
+// 	product.category === selectedCategory && product.price < maxPrice
+// );
+// renderProducts(filteredsProducts);
+// 	});
+// });
+// }
 // const clothe = product.filter(product => product.category === 'Одежда');
 // console.log(clothe);
 
